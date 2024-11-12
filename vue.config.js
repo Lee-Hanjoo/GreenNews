097e455e4f6d809397c21949ae350d9d0ccc4ac5
이한주 <gkswnehcl@gmail.com>
2024-11-12 17:53:00 +0900

    init

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  
   devServer: {
-    proxy: {
-      '/': {
-        target: 'https://api-v2.deepsearch.com',
-        changeOrigin: true,
-        pathRewrite: {'^/': ''},
-        ws: false
-      }
-    }
+    proxy: 'https://green-todo-server.vercel.app'
   }
+  // devServer: {
+  //   proxy: {
+  //     '/': {
+  //       target: 'https://api-v2.deepsearch.com',
+  //       changeOrigin: true,
+  //       pathRewrite: {'^/': ''},
+  //       ws: false
+  //     }
+  //   }
+  // }
 })
